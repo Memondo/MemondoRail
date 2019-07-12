18ffb5429bbbb62d9ef1a2800c2db63c74bea5e9
Roberto <robertogarciaaragon@gmail.com>
2019-07-12 10:47:30 +0100

    updated documentation

--- src/memondo-rail.js
+++ src/memondo-rail.js
@@ -1,57 +1,34 @@
 "use strict";
-//import { Observable, BehaviorSubject } from 'rxjs';
 exports.__esModule = true;
 var MemondoRail = /** @class */ (function () {
     function MemondoRail() {
-        this.previousY = 0;
     }
     /**
-     * Sets an intersection Observer for the reference passed
-     * @param ref - A reference of type ElementRef of the object to be intersected
-     * @param {boolean} [unobserve=true] - A value which sets if the element should be unobserved after being intersected for the first time
-     * @param {number} [treshold=1] - The percentage of pixels which must be on screen to trigger the intersection
-     * @returns {Observable<ObserverData>} - An ObserverData observable
+     * Given an HTMLElement this function will return an IntersectionObserver instance
+     * The intersection observer API see 'https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/observe'
+     *
+     *
+     * @param ref The HTMLElement which will be intersected
+     * @param callback Function to trigger when the element is being intersected, it returns data
+     * such current position, element, times, deltas etc, see API for more info
+     * @param options Options to configure the IntersectionObserver instance see API for more info
      */
-    MemondoRail.prototype.setObserver = function (ref, threshold) {
-        var _this = this;
-        if (threshold === void 0) { threshold = 0; }
-        return new Promise(function (resolve, reject) {
-            var opt = {
-                root: null,
-                rootMargin: '0px',
-                threshold: threshold
-            };
-            var previousY = 0;
-            try {
-                var observer = new IntersectionObserver(function (entries, observer) {
-                    entries.forEach(function (entry) {
-                        var currentY = entry.boundingClientRect.y;
-                        if (!entry.target.itWasInView) {
-                            if (entry.isIntersecting) {
-                                var lazyEl = entry.target;
-                                lazyEl.itWasInView = true;
-                                resolve({ direction: _this.getDirection(previousY, currentY), inView: true });
-                            }
-                            else {
-                                resolve({ direction: _this.getDirection(previousY, currentY), inView: false });
-                            }
-                        }
-                        previousY = currentY;
-                    });
-                }, opt);
-            }
-            catch (e) {
-                console.error(e);
-                reject();
-            }
-        });
+    MemondoRail.prototype.setObserver = function (ref, callback, options) {
+        var opt = options || {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0
+        };
+        var observer = new IntersectionObserver(callback, opt);
+        return observer;
     };
-    MemondoRail.prototype.getDirection = function (pre, current) {
-        var direction = 'down';
-        if (current > pre) {
-            direction = 'up';
-        }
-        return direction;
+    /**
+     * Discconnect an obersver, call this method when you dont need the element
+     * to be observed anymore
+     * @param observer Observer to be destroyed
+     */
+    MemondoRail.prototype.destroyObserver = function (observer) {
+        observer.disconnect();
     };
     return MemondoRail;
 }());
--- src/memondo-rail.ts
+++ src/memondo-rail.ts
@@ -1,8 +1,16 @@
 export class MemondoRail {
-    constructor() {
-    }
 
-    public setObserver(ref, callback, options?): IntersectionObserver {
+    /**
+     * Given an HTMLElement this function will return an IntersectionObserver instance
+     * The intersection observer API see 'https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/observe'
+     * 
+     * 
+     * @param ref The HTMLElement which will be intersected 
+     * @param callback Function to trigger when the element is being intersected, it returns data
+     * such current position, element, times, deltas etc, see API for more info
+     * @param options Options to configure the IntersectionObserver instance see API for more info
+     */
+    public setObserver(ref: HTMLElement, callback, options?): IntersectionObserver {
 
         const opt = options || {
             root: null,
@@ -15,7 +23,11 @@ export class MemondoRail {
         return observer
     }
 
-
+    /**
+     * Discconnect an obersver, call this method when you dont need the element
+     * to be observed anymore
+     * @param observer Observer to be destroyed
+     */
     public destroyObserver(observer: IntersectionObserver) {
         observer.disconnect();
     }
